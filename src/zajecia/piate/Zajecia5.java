e3193f7c3784b965c8fce7cf8889533e4ea7aaa5
unknown <lukasz.mikolaj.gronowski@gmail.com>
2017-02-06 20:56:55 +0100

    Added game with 'while' methods

--- src/zajecia/piate/Zajecia5.java
+++ src/zajecia/piate/Zajecia5.java
@@ -1,11 +1,84 @@
 package zajecia.piate;
 
+import org.w3c.dom.css.Counter;
+import zajecia.czwarte.ZadaniaZeScannerem;
+
+import java.util.Random;
+
 /**
  * Created by RENT on 2017-02-06.
  */
 public class Zajecia5 {
     public static void main(String[] args) {
-        evenMatrix(5);
+        Random random = new Random();
+        int wynik = game(random.nextInt(100));
+        wynik = game(100);
+        System.out.println("Koniec gry.");
+        System.out.println("Liczba kroków: " + wynik);
+    }
+
+    public static int[] arrayNumbersFromUser() {
+        int[] tmpArray = new int[100];
+        boolean flag = true;
+        int size = 0;
+        int numberFromUser = ZadaniaZeScannerem.getNumberFromUser("Insert next number");
+        while (flag) {
+            if (numberFromUser != 0) {
+                tmpArray[size] = numberFromUser;
+                size++;
+
+            } else {
+                flag = false;
+            }
+        }
+        return revriteArray(tmpArray, size);
+    }
+
+    public static int game(int game) {
+        boolean flag = true;
+        int counter = 0;
+        while (flag) {
+            int numberFromUser = ZadaniaZeScannerem.getNumberFromUser("Podaj liczbę");
+            counter++;
+            if (numberFromUser > game) {
+                System.out.println("Podana liczba jest za duża, spróbój ponownie");
+            } else if (numberFromUser < game) {
+                System.out.println("Podana liczba jest za mała, spróboj ponownie");
+            } else {
+                flag = false;
+            }
+        }
+        return counter;
+    }
+
+
+    public static void printArray(int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            System.out.println(array);
+        }
+    }
+
+    public static int[] revriteArray(int[] bigArray, int size) {
+        int[] array = new int[size];
+        for (int i = 0; i < size; i++) {
+            array[i] = bigArray[i];
+        }
+        return array;
+    }
+
+
+    public static int sumOfNumbersFromUser() {
+        int sum = 0;
+        boolean flag = true;
+        while (flag) {
+            int numberFromUser = ZadaniaZeScannerem.getNumberFromUser("Insert next number");
+            if (numberFromUser != 0) {
+                sum += numberFromUser;
+            } else {
+                flag = false;
+            }
+        }
+        return sum;
     }
 
     public static void triangle(int number) {
